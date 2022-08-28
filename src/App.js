915218a8325388d2fe172827078a560ed8864e1b
OziV <oz.ziv8@gmail.com>
2022-08-28 18:26:41 +0300

    WIP: update creat app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Home from "./Home";
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <Dashboard />
+      <Login />
+      <Error />
+    </div>
   );
 }
 
